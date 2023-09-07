73d427da132eda6377f1e854d31c664281a8713b
Mona Lisa Octocat <110023253+Bernkra96@users.noreply.github.com>
2023-09-07 22:21:13 +0200

    Upadete folder function

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ import fetch from 'node-fetch';
 
 // Crate folter
 
-//const folderName = './meme';
+const folderName = './meme';
 
-//fs.mkdirSync(folderName);
+try {
+  fs.rmSync(folderName, { recursive: true });
+} catch {}
+
+fs.mkdirSync(folderName);
 
 const response = await fetch(
   'https://memegen-link-examples-upleveled.netlify.app/',
@@ -17,5 +21,10 @@ const re = /<img.*\/>/gm;
 let urls = body.match(re);
 
 // console.log(typeof body);
-console.log(body);
+//console.log(body);
 console.log(urls);
+
+//console.log(urls[2].slice(10, -14));
+//console.log(urls[3].slice(10, -14));
+//console.log(urls[4].slice(10, -14));
+///console.log(urls[6].slice(10, -14));
