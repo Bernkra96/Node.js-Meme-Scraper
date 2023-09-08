4cb92c25747192cfb241df4d31c4e454a154fb2b
Mona Lisa Octocat <110023253+Bernkra96@users.noreply.github.com>
2023-09-08 13:53:45 +0200

    "objectID": "".

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ import saveImage from 'smart-image-saver';
 
 // Crate folter
 
-const folderName = 'meme';
-var filename = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10'];
+const filename = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10'];
 
 try {
   fs.rmSync('/meme', { recursive: true });
@@ -18,16 +17,7 @@ const response = await fetch(
 );
 const body = await response.text();
 const re = /<img.*\/>/gm;
-let urls = body.match(re);
-
-// console.log(typeof body);
-//console.log(body);
-//console.log(urls);
-
-//console.log(urls[2].slice(10, -14));
-//console.log(urls[3].slice(10, -14));
-//console.log(urls[4].slice(10, -14));
-///console.log(urls[6].slice(10, -14));
+const urls = body.match(re);
 
 for (let i = 0; i < 10; i++) {
   await saveImage({
