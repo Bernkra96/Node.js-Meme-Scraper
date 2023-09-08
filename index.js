96e8218fe32c95049321d732335c0dd9c5acbc20
Mona Lisa Octocat <110023253+Bernkra96@users.noreply.github.com>
2023-09-08 10:45:59 +0200

    Add Filesaver

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-import { Agent, request } from 'node:http';
 import fs from 'fs';
-import https from 'https';
 import fetch from 'node-fetch';
+import saveImage from 'smart-image-saver';
 
 // Crate folter
 
-const folderName = './meme';
+const folderName = 'meme';
+var filename = 'image01';
 
 try {
-  fs.rmSync(folderName, { recursive: true });
+  fs.rmSync('/meme', { recursive: true });
 } catch {}
 
-fs.mkdirSync(folderName);
+fs.mkdirSync('/meme');
 
 const response = await fetch(
   'https://memegen-link-examples-upleveled.netlify.app/',
@@ -22,9 +22,15 @@ let urls = body.match(re);
 
 // console.log(typeof body);
 //console.log(body);
-console.log(urls);
+//console.log(urls);
 
 //console.log(urls[2].slice(10, -14));
 //console.log(urls[3].slice(10, -14));
 //console.log(urls[4].slice(10, -14));
 ///console.log(urls[6].slice(10, -14));
+
+await saveImage({
+  url: urls[0].slice(10, -14),
+  path: 'meme/image.jpg',
+  name: 'example',
+});
